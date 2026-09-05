4346601414c82300e64f5f9d2bb80cda187cae47
Dinesh <dinesh30mane@gmail.com>
2026-09-05 14:19:39 +0530

    feat: integrate locally deployed Llama 3.2 model using Ollama

---
+++ 02-local-llm-integration-using-ollama/src/main/java/com/dineshmane/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.dineshmane.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    // invoking llama3.2 model which I've deployed locally using ollama
+    @GetMapping("/chat")
+    public String ChatWitLocalLlmModel(@RequestParam("message") String msg){
+        return chatClient.prompt(msg).call().content();
+    }
+
+
+}
